a737f75514cc386a79418597d1b849330d67cb02
gokul <websoulgokulks@gmail.com>
2024-01-07 18:17:58 +0530

    changes

--- resources/js/Components/UserSettingsDropDown.vue
+++ resources/js/Components/UserSettingsDropDown.vue
@@ -1,74 +1,83 @@
 <template>
-
-      <Menu as="div" class="relative inline-block text-left">
+    <Menu as="div" class="relative inline-block text-left">
         <div>
-          <MenuButton
-            class="inline-flex justify-center w-full px-4 py-2 text-sm font-medium text-white rounded-md bg-black/20 hover:bg-black/30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75"
-          >
-          {{ $page.props.auth.user.name }}
-            <ChevronDownIcon
-              class="w-5 h-5 ml-2 -mr-1 text-violet-200 hover:text-violet-100"
-              aria-hidden="true"
-            />
-          </MenuButton>
+            <MenuButton
+                class="inline-flex justify-center w-full px-4 py-2 text-sm font-medium text-white rounded-md bg-black/20 hover:bg-black/30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75"
+            >
+                {{ $page.props.auth.user.name }}
+                <ChevronDownIcon
+                    class="w-5 h-5 ml-2 -mr-1 text-violet-200 hover:text-violet-100"
+                    aria-hidden="true"
+                />
+            </MenuButton>
         </div>
 
         <transition
-          enter-active-class="transition duration-100 ease-out"
-          enter-from-class="transform scale-95 opacity-0"
-          enter-to-class="transform scale-100 opacity-100"
-          leave-active-class="transition duration-75 ease-in"
-          leave-from-class="transform scale-100 opacity-100"
-          leave-to-class="transform scale-95 opacity-0"
+            enter-active-class="transition duration-100 ease-out"
+            enter-from-class="transform scale-95 opacity-0"
+            enter-to-class="transform scale-100 opacity-100"
+            leave-active-class="transition duration-75 ease-in"
+            leave-from-class="transform scale-100 opacity-100"
+            leave-to-class="transform scale-95 opacity-0"
         >
-          <MenuItems
-            class="absolute right-0 w-56 mt-2 origin-top-right bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black/5 focus:outline-none"
-          >
-            <div class="px-1 py-1">
-              <MenuItem v-slot="{ active }">
-                <button
-                  :class="[
-                    active ? 'bg-violet-500 text-white' : 'text-gray-900',
-                    'group flex w-full items-center rounded-md px-2 py-2 text-sm',
-                  ]"
-                >
-                  <!-- <EditIcon
+            <MenuItems
+                class="absolute right-0 w-56 mt-2 origin-top-right bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black/5 focus:outline-none"
+            >
+                <div class="px-1 py-1">
+                    <MenuItem v-slot="{ active }">
+                        <ResponsiveNavLink
+                            :href="route('profile.edit')"
+                            :class="[
+                                active
+                                    ? 'bg-violet-500 text-white'
+                                    : 'text-gray-900',
+                                'group flex w-full items-center rounded-md px-2 py-2 text-sm',
+                            ]"
+                        >
+                            Profile
+                        </ResponsiveNavLink>
+                    </MenuItem>
+                    <MenuItem v-slot="{ active }">
+                        <button
+                            :class="[
+                                active
+                                    ? 'bg-violet-500 text-white'
+                                    : 'text-gray-900',
+                                'group flex w-full items-center rounded-md px-2 py-2 text-sm',
+                            ]"
+                        >
+                            <span class="w-5 h-5 mr-2 text-violet-400">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    strokeWidth="{1.5}"
+                                    stroke="currentColor"
+                                    className="w-6 h-6"
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15M12 9l-3 3m0 0 3 3m-3-3h12.75"
+                                    />
+                                </svg>
+                            </span>
+                            <!-- <DuplicateIcon
                     :active="active"
                     class="w-5 h-5 mr-2 text-violet-400"
                     aria-hidden="true"
                   /> -->
-                  Profile
-                </button>
-              </MenuItem>
-              <MenuItem v-slot="{ active }">
-                <button
-                  :class="[
-                    active ? 'bg-violet-500 text-white' : 'text-gray-900',
-                    'group flex w-full items-center rounded-md px-2 py-2 text-sm',
-                  ]"
-                >
-                <span class="w-5 h-5 mr-2 text-violet-400">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15M12 9l-3 3m0 0 3 3m-3-3h12.75" />
-                    </svg>
-                </span>
-                  <!-- <DuplicateIcon
-                    :active="active"
-                    class="w-5 h-5 mr-2 text-violet-400"
-                    aria-hidden="true"
-                  /> -->
-                  Logout
-                </button>
-              </MenuItem>
-            </div>
-
-          </MenuItems>
+                            Logout
+                        </button>
+                    </MenuItem>
+                </div>
+            </MenuItems>
         </transition>
-      </Menu>
+    </Menu>
+</template>
 
-  </template>
-
-  <script setup>
-  import { Menu, MenuButton, MenuItems, MenuItem } from '@headlessui/vue'
-  import { ChevronDownIcon } from '@heroicons/vue/20/solid'
-  </script>
+<script setup>
+import { Menu, MenuButton, MenuItems, MenuItem } from "@headlessui/vue";
+import { ChevronDownIcon } from "@heroicons/vue/20/solid";
+import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
+</script>
